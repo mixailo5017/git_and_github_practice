801f3524b5dba69bc58e9711f587dd5b63e5fb51
Kaidi He <kaidi@cg-la.com>
2016-04-25 12:09:39 -0400

    fix mistake on js requirement in signup page

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -295,7 +295,7 @@ class Signup extends CI_Controller
             );
             $page['scripts'] = array(
                 // 'filedrop.min.js',
-                // 'lib/filedrop-min.js',
+                'lib/filedrop-min.js',
                 'filedrop.js',
                 'lib/cropper.min.js',
                 'lib/jquery.tipsy.js'
