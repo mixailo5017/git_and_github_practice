8ca5edca1546734f57eece727ec1a9be857b80a5
Michael Pavey <michael@gvip.io>
2016-09-14 12:44:45 -0400

    Corrected code formatting for redirect controller

--- application/controllers/redirect.php
+++ application/controllers/redirect.php
@@ -2,29 +2,28 @@
     exit('No direct script access allowed');
 }
 
-class redirect extends CI_Controller
+class Redirect extends CI_Controller
 {
     protected $uriLookup = [
-    	'myvip' => 'mygvip',
-    	'myvip/myfollowers' => 'mygvip/myfollowers',
-    	'myvip/myprojects' => 'mygvip/myprojects',
-    	'myvip/myexperts' => 'mygvip/myexperts',
-    	'myvip/mydiscussions' => 'mygvip/mydiscussions'
+        'myvip' => 'mygvip',
+        'myvip/myfollowers' => 'mygvip/myfollowers',
+        'myvip/myprojects' => 'mygvip/myprojects',
+        'myvip/myexperts' => 'mygvip/myexperts',
+        'myvip/mydiscussions' => 'mygvip/mydiscussions'
     ];
 
     public function index()
     {
-	    $requestedUri = $this->uri->uri_string();
+        $requestedUri = $this->uri->uri_string();
 
-	    // You have to load url helper before you can use redirect
-	    // function in codeigniter
-	    $this->load->helper('url');
+        // You have to load url helper before you can use redirect
+        // function in codeigniter
+        $this->load->helper('url');
     
         if (array_key_exists($requestedUri, $this->uriLookup)) {
-	        redirect('/' . $this->uriLookup[$requestedUri]);	
+            redirect('/' . $this->uriLookup[$requestedUri]);
         } else {
-        	show_404($requestedUri);
+            show_404($requestedUri);
         }
-        
     }
 }
