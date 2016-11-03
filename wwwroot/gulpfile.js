52e3520b571ca16d422ec49dbe69c0b7b8c2d4a3
Michael Pavey <michael@gvip.io>
2016-11-03 14:50:06 -0400

    Added compass task to gulpfile

--- wwwroot/gulpfile.js
+++ wwwroot/gulpfile.js
@@ -4,3 +4,13 @@ var gulp = require('gulp'),
 gulp.task('default', function() {
     console.log("Hi there friend");
 });
+
+gulp.task('compass', function() {
+  gulp.src('./css/sass/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: 'css',
+      sass: 'css/sass'
+    }))
+    .pipe(gulp.dest('tmp'));
+});
