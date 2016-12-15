21dbd7933f2ef7efc96397702410b367afc4a406
Michael Pavey <michael@gvip.io>
2016-12-15 14:14:38 -0500

    Added logic to retrieve last updated date

--- application/models/projects_model.php
+++ application/models/projects_model.php
@@ -1695,7 +1695,37 @@ class Projects_model extends CI_Model {
      */
     public function get_last_updated($slug)
     {
-        return new Carbon;
+        $sql = "SELECT COALESCE(update_date.last_date, to_timestamp(created.created,'MM/DD/YYYY')) AS last_updated 
+                FROM
+                    (SELECT COALESCE(NULLIF(to_char(to_timestamp(proj.entry_date),'MM/DD/YYYY'),'01/01/1970'),'04/29/2013') AS created
+                    FROM exp_projects proj
+                      JOIN exp_members m
+                        ON (proj.uid = m.uid)
+                    WHERE slug = ?
+                     ) created
+                 FULL OUTER JOIN 
+                    (SELECT t1.last_date
+                    FROM log_projects AS t1
+                    LEFT OUTER JOIN log_projects AS t2
+                      ON t1.pid = t2.pid 
+                        AND (t1.last_date < t2.last_date 
+                         OR (t1.last_date = t2.last_date AND t1.log_id < t2.log_id))
+                    JOIN exp_projects proj 
+                      ON (proj.pid = t1.pid) 
+                    WHERE t2.pid IS NULL
+                      AND proj.slug = ?
+                    ) update_date
+                ON TRUE";
+
+        $bindings = [$slug, $slug];
+
+        $last_updated = $this
+                        ->db
+                        ->query($sql, $bindings)
+                        ->row()
+                        ->last_updated;
+
+        return new Carbon($last_updated);
     }
 
 	/**
