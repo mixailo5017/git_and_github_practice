b6b278c39c0fab3b3a913d1bbbfb4674617511cd
Michael Pavey <michael@gvip.io>
2017-02-24 16:18:54 -0500

    Bumped style.css version

--- .config/assets.master.php
+++ .config/assets.master.php
@@ -3,7 +3,7 @@
 $assets_major_version = '1';
 
 $config['assets'] = array(
-    'style.css'            => "$assets_major_version.10.1",
+    'style.css'            => "$assets_major_version.10.2",
     'script.js'            => "$assets_major_version.7.3",
     'map_search.js'        => "$assets_major_version.0.2",
     // Assets for redesigned pages
