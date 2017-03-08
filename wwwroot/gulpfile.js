8ebd0f1609d711b1c55fe573a1512dd54644d48a
Michael Pavey <mpavey@gmx.net>
2017-03-07 20:09:33 -0500

    Configured gulp to process main style files using Compass

--- wwwroot/gulpfile.js
+++ wwwroot/gulpfile.js
@@ -25,3 +25,24 @@ gulp.task('compass-dev', function() {
     }))
     .pipe(gulp.dest('css'));
 });
+
+gulp.task('main', function() {
+  gulp.src('./build/_sass/*.scss')
+    .pipe(compass({
+      config_file: './config-main.rb',
+      css: 'css',
+      sass: 'build/_sass'
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('main-dev', function() {
+  gulp.src('./build/_sass/*.scss')
+    .pipe(compass({
+      config_file: './config-main.rb',
+      css: 'css',
+      sass: 'build/_sass',
+      comments: true
+    }))
+    .pipe(gulp.dest('css'));
+});
