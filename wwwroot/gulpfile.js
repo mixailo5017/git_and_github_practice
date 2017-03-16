36f25a99c2be7a398e5ff2467db8258cc5450a10
Michael Pavey <michael@gvip.io>
2017-03-16 11:41:04 -0400

    Imported LMO gulpfile and devDependencies

--- wwwroot/gulpfile.js
+++ wwwroot/gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp'),
-    compass = require('gulp-compass');
-
-gulp.task('default', function() {
-    console.log("Hi there friend");
-});
+    compass = require('gulp-compass'),
+    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
+    csscomb = require('gulp-csscomb'),
+    please = require('gulp-pleeease'),
+    htmlv = require('gulp-html-validator'),
+    watch = require('gulp-watch'),
+    livereload = require('gulp-livereload'),
+    fileinclude = require('gulp-file-include'),
+    notify = require('gulp-notify'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    hologram = require('gulp-hologram'),
+    rename = require('gulp-rename'),
+    jshint = require('gulp-jshint'),
+    cmq = require('gulp-combine-media-queries'); // Combine CSS Media Queries at end of sheet in correct order.
 
 gulp.task('compass', function() {
   gulp.src('./css/sass/*.scss')
@@ -26,23 +37,139 @@ gulp.task('compass-dev', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('main', function() {
-  gulp.src('./build/_sass/*.scss')
-    .pipe(compass({
-      config_file: './config-main.rb',
-      css: 'css',
-      sass: 'build/_sass'
+// The next two tasks use compass to compile the v1.5 Sass — not a great idea!
+// gulp.task('main', function() {
+//   gulp.src('./build/_sass/*.scss')
+//     .pipe(compass({
+//       config_file: './config-main.rb',
+//       css: 'css',
+//       sass: 'build/_sass'
+//     }))
+//     .pipe(gulp.dest('css'));
+// });
+
+// gulp.task('main-dev', function() {
+//   gulp.src('./build/_sass/*.scss')
+//     .pipe(compass({
+//       config_file: './config-main.rb',
+//       css: 'css',
+//       sass: 'build/_sass',
+//       comments: true
+//     }))
+//     .pipe(gulp.dest('css'));
+// });
+
+//paths
+var sass_build = 'build/_sass',
+    html_build = 'build/_html/templates/*.html',
+    html_build_watch = 'build/_html/**/*.html',
+    html_output = 'html/',
+    css_output = 'css/',
+    style_css_output = 'style_guide/',
+    js_build = 'build/_js/',
+    js_output = 'js/',
+    js_output_lib = 'js/lib/';
+
+
+
+gulp.task('sass', function () {
+  return gulp.src(sass_build+'/**/*.scss')
+  .pipe(sass({
+      require:['susy'],
+      "sourcemap=none": true
     }))
-    .pipe(gulp.dest('css'));
+  .on("error", notify.onError(function (error) {
+        return  error.message;
+
+  }))
+  .pipe(please({
+    minifier:false,
+    autoprefixer:{"browsers": ["last 5 versions", "ie 9", "ios 6"]},
+    mqpacker:true
+  }))
+  // Groups media queries together in the correct order
+  .pipe(cmq())
+  .pipe(csscomb())
+  .pipe(gulp.dest(css_output))
+  .pipe(rename({
+    suffix:'.min'
+  }))
+  .pipe(please({
+    minifier:true
+  }))
+  .pipe(gulp.dest(css_output))
+  .pipe(livereload({ auto: true }));
+  });
+
+
+gulp.task('fileinclude', function() {
+  gulp.src([html_build])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(html_output))
+    .pipe(livereload({ auto: true }));
 });
 
-gulp.task('main-dev', function() {
-  gulp.src('./build/_sass/*.scss')
-    .pipe(compass({
-      config_file: './config-main.rb',
-      css: 'css',
-      sass: 'build/_sass',
-      comments: true
+gulp.task('compress', function() {
+  gulp.src(js_build+'_custom/*.js')
+    // .pipe(uglify({
+    //   compress:js_compress
+    // }))
+    .on("error", notify.onError(function (error) {
+        return  error.message;
+
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .on("error", notify.onError(function (error) {
+        return  error.message;
+
+    }))
+    .pipe(gulp.dest(js_output))
+    .pipe(rename({
+      suffix:'.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(js_output))
 });
+
+gulp.task('scripts', function() {
+  gulp.src(js_build+'_lib/*.js')
+    //.pipe(concat('plugins.js'))
+    .pipe(gulp.dest(js_output))
+    .pipe(rename({
+      suffix:'.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(js_output))
+});
+
+
+gulp.task('valid', function () {
+  gulp.src(html_output+'/*.html')
+    .pipe(htmlv())
+    .pipe(gulp.dest('./reports'));
+});
+
+gulp.task('hologram', function() {
+  gulp.src('hologram_config.yml')
+    .pipe(hologram({
+      bundler:true, 
+      logging:true
+    })
+  );
+});
+
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch(sass_build+'/**/*.scss', ['sass','hologram']);
+  gulp.watch(html_build_watch, ['fileinclude']);
+  gulp.watch(js_build+'_lib/*.js', ['scripts']);
+  gulp.watch(js_build+'_custom/*.js', ['compress']);
+});
+
+gulp.task('default',['watch']);
+gulp.task('reports', ['valid']);
