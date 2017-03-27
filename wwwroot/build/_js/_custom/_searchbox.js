ac74be82fd1f6a68b3667ce941b1ff0e66cc7267
Michael Pavey <michael@gvip.io>
2017-03-27 17:08:47 -0400

    Extracted Algolia searchbox functionality to its own files

---
+++ wwwroot/build/_js/_custom/_searchbox.js
@@ -0,0 +1,67 @@
+var searchbox = function() {
+
+	var trimHTML = require('./_trimHTML.js');
+	var client = algoliasearch("61EU8IS2O1", "fdcec7b6178f9a9c128ae03d9b7f5f40");
+	var members = client.initIndex(algoliaIndexMembers);
+	var projects = client.initIndex(algoliaIndexProjects);
+	//initialize autocomplete on search input (ID selector must match)
+	$('#aa-search-input').autocomplete(
+		{
+			hint: false,
+			debug: true,
+			keyboardShortcuts: ['/']
+		}, 
+		[
+			{
+			  source: $.fn.autocomplete.sources.hits(members, { hitsPerPage: 3 }),
+			  //value to be displayed in input control after user's suggestion selection
+			  displayKey: function(suggestion) {
+			  	return suggestion.firstname + ' ' + suggestion.lastname;
+			  },
+			  //hash of templates used when rendering dataset
+			  templates: {
+			    header: '<div class="aa-suggestions-category">' + lang['Experts'] + '</div>',
+			    //'suggestion' templating function used to render a single suggestion
+			    suggestion: function(suggestion) {
+			      var maxChars = 35;
+			      var organizationDisplayHTML = suggestion._highlightResult.organization.value;
+			      if (suggestion.organization.length > maxChars) {
+			      	organizationDisplayHTML = trimHTML(organizationDisplayHTML, maxChars);
+			      }
+			      return '<img src="' + suggestion.image + '"><span>' +
+			        suggestion._highlightResult.firstname.value + ' ' +
+			        suggestion._highlightResult.lastname.value + '</span> <span>' +
+			        organizationDisplayHTML + '</span>';
+			    },
+			    empty: '<div class="aa-suggestion aa-suggestion-empty">' + lang['NoResultsFound'] + '&nbsp;<a href="/expertise/">Advanced Search</a></div>'
+			  }
+			},
+			{
+			  source: $.fn.autocomplete.sources.hits(projects, { hitsPerPage: 3 }),
+			  //value to be displayed in input control after user's suggestion selection
+			  displayKey: 'projectname',
+			  //hash of templates used when rendering dataset
+			  templates: {
+			    header: '<div class="aa-suggestions-category">' + lang['Projects'] + '</div>',
+			    //'suggestion' templating function used to render a single suggestion
+			    suggestion: function(suggestion) {
+			      if (typeof suggestion._highlightResult.country != 'undefined') {
+			      	var country = suggestion._highlightResult.country.value;
+			      } else {
+			      	var country = '–';
+			      }
+			      return '<img src="' + suggestion.image + '"><span>' +
+			        suggestion._highlightResult.projectname.value + '</span><span>' +
+			        country + '</span>';
+			    },
+			    empty: '<div class="aa-suggestion aa-suggestion-empty">' + lang['NoResultsFound'] + '&nbsp;<a href="/projects/">Advanced Search</a></div>',
+			    footer: '<div class="aa-suggestions-footer">Powered by <img src="/images/Algolia_logo_bg-white.svg" width="48" height="17"></div>'
+			  }
+			}
+		])
+		.on('autocomplete:selected', function(event, suggestion, dataset) {
+			window.location.href = suggestion.uri;
+		});
+};
+
+module.exports = searchbox;
