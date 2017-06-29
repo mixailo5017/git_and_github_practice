4b5a478604b886536ae41f3a34fe972e3d468837
Michael Pavey <michael@gvip.io>
2017-06-29 17:11:31 -0400

    Added code to validate requests (currently inactive, since not working)

--- application/controllers/images.php
+++ application/controllers/images.php
@@ -1,5 +1,8 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 
+use League\Glide\Signatures\SignatureFactory;
+use League\Glide\Signatures\SignatureException;
+
 class Images extends CI_Controller
 {
     public function __construct()
@@ -20,10 +23,14 @@ class Images extends CI_Controller
         $testPath = ltrim(IMAGE_PATH, '/') . '/' . $imageSubdirectory . '/' . $imageFilename;
         
         if (is_file($testPath)) {
+            // Security: check valid signature (to prevent mass image-resize attacks)
+            $this->validateRequest($testPath);
+
             // Setup Glide server
             $server = League\Glide\ServerFactory::create([
                 'source' => ltrim(IMAGE_PATH, '/') . '/' . $imageSubdirectory . '/',
                 'cache' => ltrim(IMAGE_CACHE_PATH, '/'),
+                'max_image_size' => MAX_IMAGE_DIMENSIONS,
                 'defaults' => $defaults
             ]);
 
@@ -31,6 +38,26 @@ class Images extends CI_Controller
         }
         else {
             show_404();
+        }
+    }
+
+    /**
+     * Checks the image request is properly signed, to protect against
+     * mass image-resize requests
+     * @param  string $path The resource path.
+     * @return void       Stops further processing if request is invalid.
+     */
+    private function validateRequest($path) 
+    {
+        try {
+            $signkey = config_item('glide_image_signature');
+
+            // Validate HTTP signature
+            SignatureFactory::create($signkey)->validateRequest($path, $_GET);
+
+        } catch (SignatureException $e) {
+            // Handle error
+            // var_dump($e); die;
         }
     }
 }
