c9b4b97ce8faf7b3d27fb77e26ca5a059840471f
Michael Pavey <michael@gvip.io>
2017-07-07 15:00:02 -0400

    Initialize lang Array if not already defined

--- application/views/templates/_js_searchbox.php
+++ application/views/templates/_js_searchbox.php
@@ -2,6 +2,9 @@
     var algoliaIndexMembers = <?php echo "'".$this->config->item('algolia')['index_members']."'" ?>;
     var algoliaIndexProjects = <?php echo "'".$this->config->item('algolia')['index_projects']."'" ?>;
 
+    if (typeof lang === 'undefined') {
+    	var lang = new Array();
+    }
     <?php foreach ($this->lang->language['js-searchbox'] as $key => $val) { ?>
         lang['<?php echo $key ?>'] = "<?php echo addslashes($val);?>";
     <?php } ?>
