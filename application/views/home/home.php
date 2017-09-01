ce0ac39f09a4558021bd466ecaa76f9b808d8741
Michael Pavey <michael@gvip.io>
2017-09-01 16:31:37 -0400

    Switched from linking to How To page to showing new video on landing page

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -6,7 +6,7 @@
         <div class="head-cta">
             <div>
                 <a class="btn std lt-blue" href="/signup">Join for Free</a>
-                <a class="btn std clear play" href="/howto"><span>Watch It in Action</span></a>
+                <a class="btn std clear play" href="https://youtube.com/watch?v=U_xIrk7P_KY"><span>Watch It in Action</span></a>
             </div>
         </div>
     </div>
