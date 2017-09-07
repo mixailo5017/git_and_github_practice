ebb43c3cad0985e9a1ed791509a62bc207661d3e
Michael Pavey <michael@gvip.io>
2017-09-07 17:17:02 -0400

    Added webpack and plugins Configured webpack Added template for WebpackHTMLPlugin

---
+++ wwwroot/webpack.config.js
@@ -0,0 +1,59 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const inProduction = process.env.NODE_ENV === 'production';
+
+const devTool = inProduction ? 'source-map' : 'cheap-eval-source-map';
+
+// CleanWebpackPlugin: the path(s) that should be cleaned
+let pathsToClean = [
+  'js/main.*.js',
+  'js/vendor.*.js'
+];
+
+// the clean options to use
+let cleanOptions = {
+  root:     __dirname,
+  // exclude:  ['shared.js'],
+  verbose:  true,
+  dry:      false
+};
+
+const config = {
+  entry: {
+    main: './build/_js/_custom/main.js',
+    vendor: [
+      'jquery',
+      'algoliasearch',
+      'autocomplete.js'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+    new HtmlWebpackPlugin({
+      template: './build/_js/includes.ejs',
+      inject: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+  ],
+  devtool: devTool
+};
+
+// Minify JS in production
+if (inProduction) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    }
+  ));
+}
+
+module.exports = config;
