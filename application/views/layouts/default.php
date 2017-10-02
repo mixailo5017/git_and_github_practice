f300908bd4a2ea11cb055394d6e2513976e7f821
Michael Pavey <michael@gvip.io>
2017-10-02 12:49:59 -0400

    Move Optimizely snippet into head

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -6,14 +6,6 @@
     <meta name="description" content="GViP is a global platform connecting infrastructure project developers with experts. GViP has over 1400 projects and over 2000 experts. Sign up to connect with leading experts." />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!--[if lt IE 9]>
-    <script src="/js/lib/html5shiv-printshiv.min.js"></script>
-    <script src="/js/lib/html5shiv.min.js"></script>
-    <![endif]-->
-
-    <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16">
-    <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32">
-
     <link href="/css/main.css<?php echo asset_version('main.css') ?>" rel="stylesheet" type="text/css">
     <?php // Extra styles ?>
     <?php if (! empty($styles)) {
@@ -23,6 +15,17 @@
     <?php }
     } ?>
     <title><?php echo empty($title) ? SITE_NAME : $title ?></title>
+
+    <?php // Optimizely ?>
+    <script src="//cdn.optimizely.com/js/4480070248.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="/js/lib/html5shiv-printshiv.min.js"></script>
+    <script src="/js/lib/html5shiv.min.js"></script>
+    <![endif]-->
+
+    <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16">
+    <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32">
 
     <?php // Segment Analytics ?>
     <?php $this->load->view('templates/_segment_analytics', empty($page_analytics) ? array() : $page_analytics) ?>
@@ -55,9 +58,6 @@
     <?php }
 } ?>
 <?php require(FCPATH.'js/bundles.html'); ?>
-
-<?php // Optimizely ?>
-<script src="//cdn.optimizely.com/js/4480070248.js"></script>
 
 <?php // Extra stuff that we want to inject into body tags ?>
 <?php if (! empty($footer_extra)) echo $footer_extra ?>
