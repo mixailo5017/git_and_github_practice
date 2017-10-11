4d1b2ae388add866e2a5e12b3a2c6b48762c44ca
Michael Pavey <michael@gvip.io>
2017-10-11 12:11:53 -0400

    Remove logging to console

--- wwwroot/build/_js/_custom/_rekognition.js
+++ wwwroot/build/_js/_custom/_rekognition.js
@@ -21,10 +21,8 @@ function detectFaceFromBlob(imageBlob) {
     return new Promise((resolve, reject) => {
         rekognition.detectFaces(params, function(err, data) {
             if (err) {
-                console.log(err, err.stack); // an error occurred
                 reject(err);
             } else {
-                console.log(data); // successful response
                 var foundFace = data.FaceDetails.length > 0;
                 resolve(foundFace);
             }
