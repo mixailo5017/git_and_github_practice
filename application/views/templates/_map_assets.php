a9893b3643cc7af9ceca99c4a359e3a7f4b85220
Michael Pavey <michael@gvip.io>
2017-11-20 16:36:22 -0500

    Upgrade to v1.2.0 of Leaflet

--- application/views/templates/_map_assets.php
+++ application/views/templates/_map_assets.php
@@ -1,13 +1,9 @@
-<!-- Leaflet  Sept 2013 -->
-<?php echo link_tag('assets/map_resources/leaflet-0.6.4.css'); ?>
+<!-- Leaflet -->
+<?php echo link_tag('assets/map_resources/leaflet.css'); ?>
 <?php echo link_tag('assets/map_resources/L.Control.Pan.css'); ?>
 <?php echo link_tag('assets/map_resources/L.Control.Zoomslider.css'); ?>
-<!--[if lte IE 8]>
-<?php echo link_tag('assets/map_resources/leaflet-0.6.4.ie.css'); ?>
-<?php echo link_tag('assets/map_resources/L.Control.Pan.ie.css'); ?>
-<?php echo link_tag('assets/map_resources/L.Control.Zoomslider.ie.css'); ?>
-<![endif]-->
-<?php echo script_tag('assets/map_resources/leaflet-0.6.4.js');?>
+
+<?php echo script_tag('assets/map_resources/leaflet.js');?>
 <?php echo script_tag('assets/map_resources/L.Control.Pan.js');?>
 <?php echo script_tag('assets/map_resources/L.Control.Zoomslider.js');?>
-<!-- end Sept 2013 -->
+<!-- end Leaflet -->
