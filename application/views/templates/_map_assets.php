c7027caae202626e9016193ffda37c70a4e02442
Michael Pavey <michael@gvip.io>
2017-11-20 16:38:39 -0500

    Remove pan control from maps

--- application/views/templates/_map_assets.php
+++ application/views/templates/_map_assets.php
@@ -1,9 +1,7 @@
 <!-- Leaflet -->
 <?php echo link_tag('assets/map_resources/leaflet.css'); ?>
-<?php echo link_tag('assets/map_resources/L.Control.Pan.css'); ?>
 <?php echo link_tag('assets/map_resources/L.Control.Zoomslider.css'); ?>
 
 <?php echo script_tag('assets/map_resources/leaflet.js');?>
-<?php echo script_tag('assets/map_resources/L.Control.Pan.js');?>
 <?php echo script_tag('assets/map_resources/L.Control.Zoomslider.js');?>
 <!-- end Leaflet -->
