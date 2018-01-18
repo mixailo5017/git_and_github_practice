62d1871b4af05fdddbd7ad48bba63114e5fb40e2
Michael Pavey <michael@gvip.io>
2018-01-18 13:14:26 -0500

    Bump script.js version

--- .config/assets.master.php
+++ .config/assets.master.php
@@ -4,7 +4,7 @@ $assets_major_version = '1';
 
 $config['assets'] = array(
     'style.css'            => "$assets_major_version.11.2",
-    'script.js'            => "$assets_major_version.8.4",
+    'script.js'            => "$assets_major_version.8.5",
     // Assets for redesigned pages
     'main.css'            => "$assets_major_version.5.4"
 );
