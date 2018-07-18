8ba0e5b64b5c6a33d0947983d79b1680235a81aa
Michael Pavey <michael@gvip.io>
2018-07-18 17:05:40 -0400

    Clarify that results are for LALF16

--- backend/views/marketing/algosemail.php
+++ backend/views/marketing/algosemail.php
@@ -8,7 +8,8 @@
   
         
         <div id="contentwrapper" class="contentwrapper">
-			
+			<div>Showing everyone coming to LALF16 (forum ID 31).</div>
+            
             <?php foreach ($attendees as $attendee) { ?>
                 <div class="contenttitle2">
                     <h3><a target="_blank" href="https://www.gvip.io/expertise/<?= $attendee['uid'] ?>"><?= $attendee['firstname'].' '.$attendee['lastname'] ?></a></h3>
