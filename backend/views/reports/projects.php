8bbe2f8f7133c12c788699d809ae04f2b596cb81
Michael Pavey <michael@gvip.io>
2018-11-09 14:35:10 -0500

    Show average project recency in days

---
+++ backend/views/reports/projects.php
@@ -0,0 +1,23 @@
+<div class="centercontent">
+	
+	<div class="pageheader notab">
+		<h1 class="pagetitle"><?php echo $headertitle; ?></h1>
+		<span class="pagedesc">&nbsp;</span>
+		
+	</div><!--pageheader-->
+		
+		
+	<div id="contentwrapper" class="contentwrapper">
+		<div class="contenttitle2">
+			<h3>Overall</h3>
+		</div>
+		<div>Average days since projects last updated: <h2><?= $averageRecency ?></h2></div>
+		
+		<div class="contenttitle2">
+			<h3>Segments</h3>
+		</div>
+		
+		
+	</div><!--contentwrapper-->
+			
+</div>
