c5050bf9b08082cfcb26ffd8ff6be15aa7bf85c4
Michael Pavey <michael@cg-la.com>
2019-06-05 10:06:21 -0400

    Change default sort order to Highest Rated

--- application/controllers/Expertise.php
+++ application/controllers/Expertise.php
@@ -387,7 +387,7 @@ class Expertise extends CI_Controller {
     private function check_sort($value)
     {
         $allowed = array_keys($this->sort_options);
-        $default = 2;
+        $default = 4;
 
         if (in_array($value, $allowed)) {
             return $value;
