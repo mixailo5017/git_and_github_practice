833caa74697ba5bcdb02396cd597505510c97c16
Marinamicic <52413052+Marinamicic@users.noreply.github.com>
2019-09-08 21:05:07 -0400

    Update remind_sent.php

--- application/views/reminders/remind_sent.php
+++ application/views/reminders/remind_sent.php
@@ -1,6 +1,6 @@
 <section class="main-content container">
     <h1 class="h1-xl">Message sent</h1>
-    <p>Please check your email for the message that we have just sent you.<br> It will help you to reset your password.</p>
+    <p>Please check your email inbox and spam folder for the message that we have just sent you.<br> It will help you to reset your password.</p>
     <br>
     <a href="/" class="btn lt-blue">Back to the home page</a>
 </section>
