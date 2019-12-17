d9f03baf053c2282f8f608276817033a5868409f
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2019-12-17 16:40:20 -0500

    Update Expertise.php

--- application/controllers/Expertise.php
+++ application/controllers/Expertise.php
@@ -228,7 +228,12 @@ class Expertise extends CI_Controller {
 			$project = $this->expertise_model->get_organization_projects($userid);
             $breadcrumb_title = lang('B_EXPERT_ADVERTS');
             $uri_segment = 'companies';
-            $view = 'expertise/organization_view';
+            	if ($users['uid'] == 4018) {
+			$view = 'expertise/organization_view_oracle';
+			}
+	    	else {
+            		$view = 'expertise/organization_view';
+			}
 		} else {
             $page_category = 'Expert';
             $fullname = $users['firstname'] . ' ' . $users['lastname'];
