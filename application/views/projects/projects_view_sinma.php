6f4045dd8f05fe42adcbe90ca1c163e3b31b02f3
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-03-31 07:49:38 -0400

    Update projects_view_sinma.php

--- application/views/projects/projects_view_sinma.php
+++ application/views/projects/projects_view_sinma.php
@@ -1,3 +1,11 @@
+<div style="float: left; position: absolute">
+    <?php
+    $fullname = $userdata['firstname'] . $userdata['lastname'];
+    ?>
+    <iframe id="iframe" src="https://minnit.chat/GViP?embed&nickname=<?php print($fullname);?>" style="border:none;width:80%;height:500px;" allowTransparency="true"></iframe><br>
+
+</div>
+
 <div id="content" class="clearfix">
 		<div id="col2" class="projects">
 			<section class="projectdata white_box">
