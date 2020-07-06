5cb57507e949a69af5ccbd2282f00e079b23382b
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-07-06 08:43:26 -0400

    Update Stimulus.php

--- application/controllers/Stimulus.php
+++ application/controllers/Stimulus.php
@@ -13,6 +13,11 @@ class Stimulus extends CI_Controller
     {
 
         parent::__construct();
+        
+        if (!Auth::id() > 0){
+            echo ("Please Create a <a href=\"https://www.gvip.io/signup\">GViP Account</a> or <a href=\"https://www.gvip.io/login\">Sign In</a> to Access this Page <br>");
+            die('Access Denied');
+        }
 
         // If the current user doesn't have access to the forum show 404
         if (!in_array(Auth::id(), INTERNAL_USERS)) {
