c0ebe21360e432e26cd409b4461a059fb06e4b68
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-08-17 16:42:41 -0400

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -197,8 +197,8 @@ $route['stimulusBrazil/projects']               = "stimulusBrazil/index";
 
 
 //VirtualLF
-$route['virtualLF']                        = "virtualLF/show/37";
-$route['virtualLF/map']                    = "virtualLF/map/37";
+$route['virtualLF']                        = "virtualLF/show/36";
+$route['virtualLF/map']                    = "virtualLF/map/36";
 $route['virtualLF/projects/(:num)']        = "virtualLF/projects/$1";
 $route['virtualLF/experts/(:num)']         = "virtualLF/experts/$1";
 $route['virtualLF/projects']               = "virtualLF/index";
