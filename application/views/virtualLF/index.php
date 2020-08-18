b5567ea68bf8d54afde8811644ad46210e834a2a
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-08-18 11:56:59 -0400

    Update index.php

--- application/views/virtualLF/index.php
+++ application/views/virtualLF/index.php
@@ -53,7 +53,7 @@
   <section id="intro">
     <div class="intro-container" data-aos="zoom-in" data-aos-delay="100">
       <h1 class="mb-4 pb-0"> <?php echo $details['title']; ?> </h1>
-      <p class="mb-4 pb-0">July 30, 2020</p>
+      <p class="mb-4 pb-0">September 17, 2020</p>
       <!-- <a href="https://www.youtube.com/watch?v=fVkrz-W1rY4" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>-->
       <a href="#about" class="about-btn scrollto">About The Event</a>
     </div>
