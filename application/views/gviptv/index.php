919508dea354cf609bf2e91744e2d270b539600a
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-08-21 12:14:31 -0400

    Add files via upload

---
+++ application/views/gviptv/index.php
@@ -0,0 +1,299 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+
+
+<style>
+
+    body, html {
+        height: 100%;
+    }
+
+    .hero-image {
+        /* Use "linear-gradient" to add a darken background effect to the image (photographer.jpg). This will make the text easier to read */
+        background-image: url("https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Global+Banner.jpg");
+
+        /* Set a specific height */
+        height: 50%;
+
+        /* Position and center the image to scale nicely on all screens */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+    }
+
+    /* Place text in the middle of the image */
+    .hero-text {
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+    }
+
+    .center {
+        display: block;
+        margin-top: auto;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: auto;
+    }
+</style>
+
+<a href="https://gvip.io/virtualLF">
+    <div class="hero-image">
+        <div class="hero-text">
+        </div>
+    </div>
+</a>
+
+<div style="padding-top: 50px"></div>
+
+<!-- Grid row -->
+<div style="padding-right: 2%; padding-left: 2%" class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-12 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/DwWyTQHcSSw"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/1+Penna.png" alt="video"
+                data-toggle="modal" data-target="#modal1"></a>
+        <h3>David Penna</h3>
+        <p>Senior Vice President, Office of Strategic Initiatives, U.S. International Development Finance Corporation (DFC)</p>
+        <p>in conversation with Norman Anderson, President, and CEO, CG-LA Infrastructure</p>
+        <p><strong>The U.S. Development Corporation (DFC)</strong></p>
+        <p><strong>Funding for Reshoring Critical Capabilities</strong></p>
+
+    </div>
+    <!-- Grid column -->
+
+
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal6" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/VqVZKK6jUc4"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/2.+French+Hill.png" alt="video"
+                data-toggle="modal" data-target="#modal6"></a>
+        <h3>French Hill</h3>
+        <p>U.S. House of Representatives from Arkansas's 2nd district</p>
+        <p>in conversation with Norman Anderson, President, and CEO, CG-LA Infrastructure</p>
+        <p><strong>View from the White House on Infrastructure  and Stimulus</strong></p>
+    </div>
+    <!-- Grid column -->
+
+
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/F38myQfNIMA"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/3.+Leadership+Panel.png" alt="video"
+                data-toggle="modal" data-target="#modal4"></a>
+
+        <h3>Leadership Panel: What the Infrastructure Community Needs from Washington</h3>
+        <p>Mike Johnson, Sr. Vice President - Infrastructure Market and Strategy, Kiewit</p>
+        <p>Pierce Homer, Director, Moffat & Nichol (former Sec. of Transportation, Virginia)</p>
+        <p>K. N. Gunalan, Senior Vice President, Alternative Delivery, Americas, AECOM (current ASCE 2020 President)</p>
+        <p>Norman Anderson, President, and CEO, CG-LA Infrastructure</p>
+
+    </div>
+    <!-- Grid column -->
+
+</div>
+<!-- Grid row -->
+
+
+<div style="padding-right: 2%; padding-left: 2%" class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-12 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0Klp_VSCp0w"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/4+Army+Corps.png" alt="video"
+                data-toggle="modal" data-target="#modal1"></a>
+        <h3>Army Corps</h3>
+        <p>U.S. Army Corps of Engineers - Meeting Covid Challenges with Priority Projects</p>
+        <p>Peter Dodgion, Asset Management Program Manager, U.S. Army Corps of Engineers</p>
+    </div>
+    <!-- Grid column -->
+
+
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/OGbw2-aUhZU"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/5+Dry+Power.png" alt="video"
+                data-toggle="modal" data-target="#modal6"></a>
+        <h3>Dry Powder & the Build America Bureau</h3>
+        <p>Roger Bohnert, Director of Business Outreach, Build America Bureau, U.S. DOT</p>
+
+    </div>
+    <!-- Grid column -->
+
+
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <!--Content-->
+                <div style="top: 10em" class="modal-content">
+                    <!--Body-->
+                    <div class="modal-body mb-0 p-0">
+                        <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/O49MWYEgeYw"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1" src="https://d2huw5an5od7zn.cloudfront.net/gviptv/images/6.+Petreus.png" alt="video"
+                data-toggle="modal" data-target="#modal4"></a>
+
+        <h3>General David H. Petraeus</h3>
+        <p>General David H. Petraeus AO, MSC,</p>
+        <p>Former Director of the Central Intelligence Agency</p>
+        <p>in conversation with Norman Anderson, President, and CEO, CG-LA Infrastructure</p>
+        <p><strong>Filmed live at the North American Infrastructure Leadership Forum, Washington DC, 2019</strong></p>
+
+    </div>
+    <!-- Grid column -->
+
+</div>
+
+
+
+<script>
+    $('#modal1').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+    });
+
+    $('#modal6').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+    });
+
+    $('#modal4').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+    });
+
+    $('#modal2').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal2 iframe').attr("src", $("#modal2 iframe").attr("src"));
+    });
+
+    $('#modal3').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal3 iframe').attr("src", $("#modal3 iframe").attr("src"));
+    });
+
+    $('#modal5').on('hidden.bs.modal', function (e) {
+        // do something...
+        $('#modal5 iframe').attr("src", $("#modal5 iframe").attr("src"));
+    });
+
+</script>
