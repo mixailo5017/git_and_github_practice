47ebf42167adda09217d41e8c16be54b7bdc509d
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-09-09 18:37:52 -0400

    new homepage test

--- application/views/jobs/index.php
+++ application/views/jobs/index.php
@@ -1,33 +1,145 @@
-<?php
-foreach($allProj as $key => $orgexp)
-{
-    $jobscreated =  $model_obj->get_jobs_created($orgexp['pid']);
-    echo $jobscreated.'<br>';
-}
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<title>Untitled</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/styles.min.css">
+<link rel="stylesheet" href="https://daneden.github.io/animate.css/animate.min.css">
+</head>
 
+<body style="border-style: none;border-radius: 0px;">
+<div class="row" style="background-color: #DCE8F0;">
+    <div class="col-md-6" style="background: url(https://d2huw5an5od7zn.cloudfront.net/homepage/imageedit_2_6332446049.png) center / contain no-repeat;height: 220px;">
+        <div></div>
+    </div>
+    <div class="col-md-6" style="text-align: right;background-color: #DCE8F0;padding: 15px 50px;">
+        <h1>GViP</h1>
+        <p style="font-size: 20px;">The premier infrastructure intelligence and networking hub.</p><button class="btn btn-primary" type="button" style="background-color: #2274A5;border-width: 1px;border-radius: 0px;">Log In</button></div>
+</div>
+<div>
+    <div class="container">
+        <div class="row" style="padding: 20px;">
+            <div class="col-md-12">
+                <h1 style="text-align: center;font-size: 30px;">Build your membership in the Global Infrastructure Community</h1>
+            </div>
+        </div>
+        <div class="row" style="padding: 0px;">
+            <div class="col-md-4" style="text-align: center;padding: 15px 15px;"><button class="btn btn-primary" type="button" style="background-color: #2274A5;border-width: 1px;border-radius: 0px;text-align: center;height: 50px;">Sign Up</button></div>
+            <div class="col-md-4" style="text-align: center;padding: 15px 15px;"><button class="btn btn-primary" type="button" style="background-color: #2274A5;border-width: 1px;border-radius: 0px;text-align: center;height: 50px;">Watch it in Action</button></div>
+            <div class="col-md-4" style="text-align: center;padding: 15px 15px;"><button class="btn btn-primary" type="button" style="background-color: #2274A5;border-width: 1px;border-radius: 0px;text-align: center;height: 50px;">Request a Demo</button></div>
+        </div>
+    </div>
+</div>
+<div class="row" style="background-color: #E9F4E1;">
+    <div class="col-md-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center" style="height: 350px;">
+        <div></div><iframe allowfullscreen="" frameborder="0" src="https://www.youtube.com/embed/NcOPnfWD55Q?autoplay=1&amp;mute=1" width="560" height="315"></iframe></div>
+    <div class="col-md-6 align-self-center" style="text-align: right;background-color: #E9F4E1;padding: 0px 50px;">
+        <h4 class="d-flex d-lg-flex justify-content-lg-end align-items-lg-end" style="color: #2274A5;text-align: left;">Strategic Project Connections Globally (Powered by GViP AI+)</h4>
+        <p style="font-size: 20px;text-align: left;">To date, GViP has connected 3737 experts with 3410 projects across 163 countries! That's a total project value of $7.4 trillion, creating an estimated 82 million jobs.</p>
+    </div>
+</div>
+<div>
+    <div class="container">
+        <div class="row" style="padding: 25px;">
+            <div class="col-md-12">
+                <h4>The world of infrastructure is at your fingertips</h4>
+                <h2 style="color: #2274A5;">Solutions that expand your network and grow your business.</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="d-flex m-auto align-items-lg-center" style="padding: 10px;">
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / auto no-repeat;height: 78px;">
+                        <div class="feature-wrap" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / contain no-repeat;"></div>
+                    </div>
+                    <h6 class="text-center d-flex flex-column justify-content-center align-items-center" style="background-color: #f2f2f2;text-align: center;height: 71px;width: 300px;">Search for a Project&nbsp;</h6>
+                </div>
+                <div class="d-flex m-auto align-items-lg-center" style="padding: 10px;">
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / auto no-repeat;height: 78px;">
+                        <div class="feature-wrap" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / contain no-repeat;"></div>
+                    </div>
+                    <h6 class="text-center d-flex d-md-flex flex-column justify-content-center align-items-center justify-content-md-center align-items-md-center justify-content-lg-center" style="background-color: #f2f2f2;text-align: center;height: 71px;width: 300px;">Find an infrastructure expert</h6>
+                </div>
+                <div class="d-flex m-auto align-items-lg-center" style="padding: 10px;">
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / auto no-repeat;height: 78px;">
+                        <div class="feature-wrap" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / contain no-repeat;"></div>
+                    </div>
+                    <h6 class="text-center d-flex flex-column justify-content-center align-items-center" style="background-color: #f2f2f2;text-align: center;height: 71px;width: 300px;">Attend an infrastructure leadership forum</h6>
+                </div>
+                <div class="d-flex m-auto align-items-lg-center" style="padding: 10px;">
+                    <div class="col-md-4 col-sm-6 wow fadeInDown" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / auto no-repeat;height: 78px;">
+                        <div class="feature-wrap" style="background: url(https://d2huw5an5od7zn.cloudfront.net/onlineforum/assets/img/Agenda/CGLA_newlogo.jpg) center / contain no-repeat;"></div>
+                    </div>
+                    <h6 class="text-center d-flex flex-column justify-content-center align-items-center" style="background-color: #f2f2f2;text-align: center;height: 71px;width: 300px;">Hear from experts on GViP Tv</h6>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="https://d2huw5an5od7zn.cloudfront.net/gviplogo.png" style="width: 600px;">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row" style="background-color: #E9F4E1;">
+    <div class="col-md-6 align-self-center" style="text-align: right;background-color: #E9F4E1;padding: 0px 50px;">
+        <p style="font-size: 20px;text-align: center;"><strong>Post your project</strong>&nbsp;and find the investors, engineers, consultants, and technology you need to push your project forward.</p>
+    </div>
+    <div class="col-md-6" style="height: 220px;">
+        <div class="d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center" style="height: 220px;"><button class="btn btn-primary" type="button" style="background-color: transparent;color: #578661;border-width: 2px;border-color: #578661;width: 240px;height: 83px;font-size: 28px;">Post Your Project</button></div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 align-self-center" style="text-align: right;padding: 0px 50px;">
+        <p style="font-size: 20px;text-align: center;"><strong>Search projects</strong>&nbsp;to find big opportunities, compare project bench marks, and analyze project data. Filter the project database by country, stage, sector, sub-sector, and more.</p>
+    </div>
+    <div class="col-md-6" style="padding: 15px 15px;">
+        <div class="text-center d-flex justify-content-center justify-content-lg-center align-items-lg-center" style="height: 50px;">
+            <h4 style="font-size: 21px;">Suggested Searches</h4>
+        </div>
+        <div class="d-flex justify-content-lg-center align-items-lg-center"><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Energy</button><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Logistics</button>
+            <button
+                class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Water</button>
+        </div>
+        <div class="d-flex justify-content-lg-center align-items-lg-center"><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Conceptual</button><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Feasibility</button>
+            <button
+                class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Procurement</button>
+        </div>
+        <div class="d-flex justify-content-lg-center align-items-lg-center"><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">U.S.</button><button class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Canada</button>
+            <button
+                class="btn btn-primary" type="button" style="margin: 5px;color: #313437;background-color: #f2f2f2;border-style: none;">Brazil</button>
+        </div>
+    </div>
+</div>
+<div class="row" style="background-color: #DCE8F0;">
+    <div class="col-md-6 text-center d-flex justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" style="text-align: right;background-color: #DCE8F0;padding: 15px 50px;">
+        <p class="align-content-center" style="font-size: 20px;text-align: center;"><strong>Connect with experts&nbsp;</strong>to build your network in the infrastructure community and access key project insights.</p>
+    </div>
+    <div class="col-md-6">
+        <div></div>
+        <div class="text-center d-flex justify-content-center justify-content-lg-center align-items-lg-center" style="height: 50px;">
+            <h4 style="font-size: 21px;">Suggested Experts</h4>
+        </div>
+        <div class="text-center d-flex justify-content-center justify-content-lg-center align-items-lg-center"><img src="https://d2huw5an5od7zn.cloudfront.net/homepage/Screen+Shot+2020-09-02+at+2.49.55+PM.png" style="width: 370px;"></div>
+    </div>
+</div>
+<div class="row" style="background-color: #E9F4E1;">
+    <div class="col-md-6 align-self-center" style="text-align: right;padding: 0px 50px;">
+        <p style="font-size: 20px;text-align: center;"><strong>Attend a virtual infrastructure leadership forum</strong>&nbsp;for on-demand intelligence and hear directly from project owners and leading, industry CEO's</p>
+    </div>
+    <div class="col-md-6" style="padding: 15px 15px;">
+        <div class="text-center d-flex justify-content-center justify-content-lg-center align-items-lg-center" style="height: 50px;">
+            <h4 style="font-size: 21px;">Suggested Content</h4>
+        </div>
+        <div class="d-flex justify-content-lg-center align-items-lg-center"><img src="https://d2huw5an5od7zn.cloudfront.net/homepage/Screen+Shot+2020-09-02+at+3.02.42+PM.png" style="width: 384px;"></div>
+    </div>
+</div>
 
-
-$features = array();
-foreach($stimMap as $project => $orgexp)
-{
-    $features[] = array(
-        'type' => 'Feature',
-        'properties' => array('id' => $orgexp['pid'], 'jobs' =>  $model_obj->get_jobs_created($orgexp['pid']), 'location'=> $orgexp['location'], 'description'=> $orgexp['description'], 'projectname'=> $orgexp['projectname'], 'sector'=> $orgexp['sector'], 'stage'=> $orgexp['stage'], 'sponsor'=> $orgexp['sponsor'], 'subsector'=> $orgexp['subsector'], 'slug'=> $orgexp['slug'], 'projectphoto'=> $orgexp['projectphoto'], 'description'=> $orgexp['description'], 'country'=> $orgexp['country'], 'totalbudget'=> $orgexp['totalbudget']),
-        'geometry' => array(
-            'type' => 'Point',
-            'coordinates' => array(
-                $orgexp['lng'] + (rand(-1000,1000)*.00001),
-                $orgexp['lat'] + (rand(-1000,1000)*.00001),
-                1
-            ),
-        ),
-    );
-}
-$new_data = array(
-    'type' => 'FeatureCollection',
-    'features' => $features,
-);
-$final_data = json_encode($new_data, JSON_PRETTY_PRINT);
-
-echo $final_data;
-?>
+<div class="row" style="background-color: #FFFAE7;">
+    <div class="col text-center d-lg-flex justify-content-center align-items-center align-content-center justify-content-lg-center" style="height: 200px;">
+        <div>
+            <h1 style="color: #2274A5;">Finding infrastructure experts and intelligence is easier than ever on GViP</h1><button class="btn btn-primary" type="button" style="background-color: transparent;color: #2274A5;height: 53px;width: 175px;font-size: 27px;border-width: 2px;border-style: solid;">Get Started</button></div>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/script.min.js"></script>
+</body>
