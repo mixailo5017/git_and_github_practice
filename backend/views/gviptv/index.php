c4b5b6302b2d5d7ff36e45a82610ca3f99853d53
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-09-30 13:32:09 -0400

    Update index.php

--- backend/views/gviptv/index.php
+++ backend/views/gviptv/index.php
@@ -13,14 +13,9 @@
             <a class="close"></a>
             <p></p>
         </div>
-        <div class="tableoptions">
-            <button class="deletebutton radius3" title="Delete Selected" name="dyntable_forums" id="#/admin.php/forums/destroy">Delete Selected</button> &nbsp;
-        </div><!--tableoptions-->
 
         <table cellpadding="0" cellspacing="0" border="0" class="stdtable" id="dyntable_forums">
             <colgroup>
-                <col class="con0" style="width: 4%" />
-                <col class="con1" />
                 <col class="con0" />
                 <col class="con1" />
                 <col class="con0" />
@@ -29,13 +24,6 @@
             </colgroup>
             <thead>
             <tr>
-                <th class="head0 nosort" align="center"><span class="center">
-                        <?php echo form_checkbox(array(
-                            'id' => 'select_all_header',
-                            'name' => 'select_all_header',
-                            'class' => 'checkall'
-                        )); ?></span>
-                </th>
                 <th class="head1">ID</th>
                 <th class="head0">Title</th>
                 <th class="head1">Category</th>
@@ -45,13 +33,6 @@
             </thead>
             <tfoot>
             <tr>
-                <th class="head0 nosort" align="center"><span class="center">
-                        <?php echo form_checkbox(array(
-                            'id' => 'select_all_header',
-                            'name' => 'select_all_header',
-                            'class' => 'checkall'
-                        )); ?></span>
-                </th>
                 <th class="head1">ID</th>
                 <th class="head0">Title</th>
                 <th class="head1">Category</th>
@@ -81,7 +62,7 @@
                     <td><?php echo ($row['status'] == '1') ? 'Active' : 'Draft'; ?></td>
 
                     <td>
-                        <a class="delete" href="" name="<?php echo $row['id']; ?>" id="#/admin.php/forums/destroy">Delete</a>
+                        <a class="delete" href="#/admin.php/forums/destroy/<?php echo $row['id']; ?>">Delete</a>
                     </td>
                 </tr>
                 <?php
