0b46f2919e7e0cd1dc523307a3ae36d79cb43b28
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-09-30 16:38:50 -0400

    Add view files for backend gviptv

---
+++ backend/views/gviptv/edit.php
@@ -0,0 +1,25 @@
+<div class="centercontent">
+    <div class="pageheader notab">
+        <h1 class="pagetitle">Edit Video</h1>
+        <span class="pagedesc">&nbsp;</span>
+        <a class="right goback" style="margin-right:20px;" href="/admin.php/gviptv"><span>Back</span></a>
+    </div><!-- pageheader -->
+
+    <div id="contentwrapper" class="contentwrapper">
+        <div class="notibar_add" style="display:none">
+            <a class="close"></a>
+            <p></p>
+        </div>
+
+        <div class="widgetcontent">
+            <div id="tabs">
+                <ul>
+                    <li><a href="#general">General</a></li>
+                </ul>
+                <div id="general">
+                    <?php $this->load->view('gviptv/_general', $details); ?>
+                </div>
+            </div><!-- tabs -->
+        </div><!-- widgetcontent -->
+    </div><!-- contentwrapper -->
+</div><!-- centercontent -->
