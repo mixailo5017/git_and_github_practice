666c10c6defcad8cac8ee239aac0092624d3c6f6
johnbrisbane <52230736+johnbrisbane@users.noreply.github.com>
2020-09-30 13:49:00 -0400

    Update index.php

--- backend/views/gviptv/index.php
+++ backend/views/gviptv/index.php
@@ -13,9 +13,14 @@
             <a class="close"></a>
             <p></p>
         </div>
+        <div class="tableoptions">
+            <button class="deletebutton radius3" title="Delete Selected" name="dyntable_forums" id="#/admin.php/gviptv/destroy">Delete Selected</button> &nbsp;
+        </div><!--tableoptions-->
 
         <table cellpadding="0" cellspacing="0" border="0" class="stdtable" id="dyntable_forums">
             <colgroup>
+                <col class="con0" style="width: 4%" />
+                <col class="con1" />
                 <col class="con0" />
                 <col class="con1" />
                 <col class="con0" />
@@ -24,6 +29,13 @@
             </colgroup>
             <thead>
             <tr>
+                <th class="head0 nosort" align="center"><span class="center">
+                        <?php echo form_checkbox(array(
+                            'id' => 'select_all_header',
+                            'name' => 'select_all_header',
+                            'class' => 'checkall'
+                        )); ?></span>
+                </th>
                 <th class="head1">ID</th>
                 <th class="head0">Title</th>
                 <th class="head1">Category</th>
@@ -33,6 +45,13 @@
             </thead>
             <tfoot>
             <tr>
+                <th class="head0 nosort" align="center"><span class="center">
+                        <?php echo form_checkbox(array(
+                            'id' => 'select_all_header',
+                            'name' => 'select_all_header',
+                            'class' => 'checkall'
+                        )); ?></span>
+                </th>
                 <th class="head1">ID</th>
                 <th class="head0">Title</th>
                 <th class="head1">Category</th>
@@ -46,6 +65,12 @@
 
                 ?>
                 <tr>
+                    <td align="center"><span class="center"><?php echo form_checkbox(array(
+                                'id' => 'select_' . $row['id'],
+                                'name' =>' select_' . $row['id'],
+                                'value' => $row['id']
+                            )); ?></span></td>
+
                     <td><?php echo $row['id']; ?></td>
                     <td>
                         <a href="/<?php echo index_page(); ?>/forums/edit/<?php echo $row['id']; ?>">
@@ -56,7 +81,7 @@
                     <td><?php echo ($row['status'] == '1') ? 'Active' : 'Draft'; ?></td>
 
                     <td>
-                        <a class="delete" href="gviptv/destroy/<?php echo $row['id']; ?>">Delete</a>
+                        <a class="delete" href="gviptv/destroy/<?php echo $row['id']; ?>" >Delete</a>
                     </td>
                 </tr>
                 <?php
